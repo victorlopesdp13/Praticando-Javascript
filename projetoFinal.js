13739bb9c8adcafe72d3ef6e00db14e8fe8995dc
Victor Lopes <victormoreiralopes@hotmail.com>
2023-02-17 14:17:26 -0300

    Função de receber pedido

--- projetoFinal.js
+++ projetoFinal.js
@@ -2,10 +2,28 @@
 const prompt = require("prompt-sync")({sigint: true}); 
 
 // Receber o pedido
+function recebePedido (){
+    let pedido = {
+        sabor: null,
+        confeitos: null
+    };
+    // Qual o sabor do bolo?
+    pedido.sabor = prompt("Qual o sabor do bolo? ")
+    
+    // Quais confeitos deseja?
+    pedido.confeitos = prompt("Qual confeitos deseja? ")
+   
+
+    return pedido;
+    
+};
 
     
 // Preparar o pedido
-
+    // Separar ingredientes
+    // Misturar Bolo
+    // Colocar no forno
+    // Confeitar
     
 // Calcular o preço
     // Valor dos ingredientes
@@ -22,3 +40,7 @@ const prompt = require("prompt-sync")({sigint: true});
 // Receber dinheiro
     // Receber dinheiro do cliente
     // Fechar pedido e agradecer ao cliente
+
+
+let pedido = recebePedido();
+console.log(pedido)
