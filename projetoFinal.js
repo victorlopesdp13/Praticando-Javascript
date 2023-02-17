b2dd883f0b7aa18f394de99b618ecca5e2291e09
Victor Lopes <victormoreiralopes@hotmail.com>
2023-02-17 14:04:04 -0300

    instalando pacote

---
+++ projetoFinal.js
@@ -0,0 +1,24 @@
+// Biblioteca para capturar inputs de texto do usuário no console
+const prompt = require("prompt-sync")({sigint: true}); 
+
+// Receber o pedido
+
+    
+// Preparar o pedido
+
+    
+// Calcular o preço
+    // Valor dos ingredientes
+    // Valor da confeiteira
+    // Valor das depesas de produção
+    // Valor dos gastos fixos
+    // Valor do lucro
+    // Retornar preço final
+
+// Entregar pedido
+    // Exibir/ entregar bolo para cliente
+    // exbir preço para o cliente
+
+// Receber dinheiro
+    // Receber dinheiro do cliente
+    // Fechar pedido e agradecer ao cliente
